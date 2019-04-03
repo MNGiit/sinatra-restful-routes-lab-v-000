46dcd113eff8d6118c7dfa2f4662bca50d134b2f
Mohammed Noori <kmanoorib@gmail.com>
2019-04-03 06:22:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   # end
   
   post '/recipes' do
-    @recipe = Recipe.create(:title => params[:title], :ingredents => params[:ingredients], :cook_time => params[:cook_time]) # doesn't work
+    @recipe = Recipe.create(:name => params[:name], :ingredents => params[:ingredients], :cook_time => params[:cook_time]) # doesn't work
     # @recipe = Recipe.create(params)
     
     redirect to "/recipes/#{@recipe.id}"
